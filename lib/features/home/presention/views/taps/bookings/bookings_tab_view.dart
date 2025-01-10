05eafea19bb3fe3e35524c5ffe7eda643d685136
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-01-11 00:02:05 +0200

    -Added the tabs

---
+++ lib/features/home/presention/views/taps/bookings/bookings_tab_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BookingsTabView extends StatelessWidget {
+  const BookingsTabView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/features/home/presention/views/taps/users/users_tab_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UsersTabView extends StatelessWidget {
+  const UsersTabView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
