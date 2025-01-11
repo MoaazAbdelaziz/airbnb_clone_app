6912820f2dbbd83ab7eb18830a4d05497201edfb
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-01-12 01:08:13 +0200

    -Wrapped it with bloc provider

--- lib/features/home/presention/views/taps/bookings/bookings_tab_view.dart
+++ lib/features/home/presention/views/taps/bookings/bookings_tab_view.dart
@@ -1,13 +1,19 @@
+import 'package:airbnb_clone_app/features/home/presention/views/taps/bookings/manger/bookings_tab_cubit.dart';
 import 'package:airbnb_clone_app/features/home/presention/views/taps/bookings/widgets/bookings_tab_view_body.dart';
+import 'package:airbnb_clone_app/injectables.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BookingsTabView extends StatelessWidget {
   const BookingsTabView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: BookingsTabViewBody(),
+    return BlocProvider(
+      create: (context) => getIt<BookingsTabCubit>(),
+      child: Scaffold(
+        body: BookingsTabViewBody(),
+      ),
     );
   }
 }
