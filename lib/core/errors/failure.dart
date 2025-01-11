9e5aea7839885ec9f190e992da2f0f842a302315
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-01-11 22:08:02 +0200

    -Finished the failure cases

--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,9 +1,48 @@
+import 'package:dio/dio.dart';
+
 abstract class Failure {
   final String message;
 
   const Failure(this.message);
 }
 
-class NetworkFailure extends Failure {
-  NetworkFailure(super.message);
+class ServerFailure extends Failure {
+  ServerFailure(super.message);
+
+  factory ServerFailure.fromDioException(DioException dioException) {
+    switch (dioException.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection timeout with ApiServer');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send timeout with ApiServer');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive timeout with ApiServer');
+      case DioExceptionType.badCertificate:
+        return ServerFailure('Bad Certificate with ApiServer');
+      case DioExceptionType.badResponse:
+        return ServerFailure.fromResponse(
+            dioException.response!.statusCode!, dioException.response!.data);
+      case DioExceptionType.cancel:
+        return ServerFailure('Request with ApiService was Canceled');
+      case DioExceptionType.connectionError:
+        return ServerFailure('Error Connection with ApiServer');
+      case DioExceptionType.unknown:
+        if (dioException.message!.contains('SocketException')) {
+          return ServerFailure('No Internet Connection');
+        }
+        return ServerFailure('Unexpected Error, Please Try again later');
+    }
+  }
+
+  factory ServerFailure.fromResponse(int statusCode, dynamic response) {
+    if (statusCode == 400 || statusCode == 401 || statusCode == 403) {
+      return ServerFailure(response['error']['message']);
+    } else if (statusCode == 404) {
+      return ServerFailure('Your Request is not Found, Please try later');
+    } else if (statusCode == 500) {
+      return ServerFailure('Internal Server Error, Please try later');
+    } else {
+      return ServerFailure('Opps There was an Error, Please try again');
+    }
+  }
 }
