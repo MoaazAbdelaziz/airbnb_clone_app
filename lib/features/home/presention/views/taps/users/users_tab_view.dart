5920e08f2d20ce9b0cc4210f1c831dcf41995b46
MoaazAbdelaziz <moaazwork1@gmail.com>
2025-01-11 22:11:18 +0200

    -Created user tab view

--- lib/features/home/presention/views/taps/users/users_tab_view.dart
+++ lib/features/home/presention/views/taps/users/users_tab_view.dart
@@ -1,10 +1,19 @@
+import 'package:airbnb_clone_app/features/home/presention/views/taps/users/manger/cubit/users_tab_cubit.dart';
+import 'package:airbnb_clone_app/features/home/presention/views/taps/users/widgets/user_tab_view_body.dart';
+import 'package:airbnb_clone_app/injectables.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class UsersTabView extends StatelessWidget {
   const UsersTabView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return BlocProvider<UsersTabCubit>(
+      create: (context) => getIt<UsersTabCubit>(),
+      child: Scaffold(
+        body: UsersTabViewBody(),
+      ),
+    );
   }
 }
